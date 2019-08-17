09e2eee62d0b884b57b204473aa4faa6201b0e52
JXY001A <jxy001a@aliyun.com>
2019-08-17 15:45:06 +0800

    添加autoProfiexh插件保证兼容性

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,12 @@
  * @author: JXY
  * @Date: 2019-08-16 23:44:09
  * @Email: JXY001a@aliyun.com
- * @LastEditTime: 2019-08-16 23:46:24
+ * @LastEditTime: 2019-08-17 15:43:33
  */
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        /* https://github.com/browserslist/browserslist#best-practices 配置需要兼容的浏览器列表 */
+        // 自动添加css 属性前缀，保证兼容性
+        require('autoprefixer')("last 100 versions")
     ]
 }
