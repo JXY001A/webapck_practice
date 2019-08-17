c25f1906b597afabe9b8e8f2415963c90ad40c61
JXY001A <jxy001a@aliyun.com>
2019-08-17 16:04:09 +0800

    添加记录打包过程命令用于分析构建产物

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,11 @@
  * @author: JXY
  * @Date: 2019-08-16 23:44:09
  * @Email: JXY001a@aliyun.com
- * @LastEditTime: 2019-08-17 15:43:33
+ * @LastEditTime: 2019-08-17 15:46:18
  */
 module.exports = {
     plugins: [
-        /* https://github.com/browserslist/browserslist#best-practices 配置需要兼容的浏览器列表 */
+        /* https://github.com/browserslist/browserslist#best-practices 配置需指定要兼容的浏览器列表 */
         // 自动添加css 属性前缀，保证兼容性
         require('autoprefixer')("last 100 versions")
     ]
